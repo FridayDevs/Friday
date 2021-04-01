072934de5e016da75ed71141f4a90044c25dd92d
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-04-01 17:28:17 +0530

    Update help.py

--- assistant/help.py
+++ assistant/help.py
@@ -1,19 +1,13 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-#
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-#
-# All rights reserved.
 
 
-from main_startup.core.decorators import friday_on_cmd
+
+from main_startup.core.decorators import friday_on_cmd as skem
 from main_startup.core.startup_helpers import run_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 from main_startup.__main__ import bot
 
 
-@friday_on_cmd(
+@skem(
     ["help", "helper"],
     cmd_help={
         "help": "Gets Help Menu",
